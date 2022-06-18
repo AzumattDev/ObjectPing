f541ea15f8a519e93cea1bba43d6bf533b59306b
AzumattDev <azumattgaming@gmail.com>
2022-06-18 09:09:07 -0400

    Initial commit

---
+++ Patches.cs
@@ -0,0 +1,100 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ObjectPing;
+
+[HarmonyPatch(typeof(Player), nameof(Player.OnSpawned))]
+static class PlayerOnSpawnedPatch
+{
+    static void Postfix(Player __instance)
+    {
+        if (ObjectPingPlugin.Placementmarkercopy != null) return;
+        if (__instance.m_placementMarkerInstance == null)
+            __instance.m_placementMarkerInstance =
+                Object.Instantiate(__instance.m_placeMarker, Hud.m_instance.m_rootObject.transform);
+        ObjectPingPlugin.Placementmarkercopy = Object.Instantiate(__instance.m_placementMarkerInstance,
+            Hud.m_instance.m_rootObject.transform);
+        ObjectPingPlugin.Placementmarkercopy.AddComponent<TimedDestruction>();
+        TimedDestruction? t = ObjectPingPlugin.Placementmarkercopy.GetComponent<TimedDestruction>();
+        t.m_triggerOnAwake = true;
+        t.m_timeout = 5f;
+    }
+}
+
+[HarmonyPatch(typeof(Hud), nameof(Hud.UpdateCrosshair))]
+static class PlayerUpdatePatch
+{
+    private static Camera? _cam;
+    private static int _layermask = 1;
+    private const int Layer1 = 1;
+    private const int Layer2 = 2;
+    private const int Layer3 = 4;
+    private const int Layer4 = 5;
+    private const int Layer5 = 8;
+    private const int Layer6 = 13;
+    private const int Layer7 = 14;
+    private const int Layer8 = 16;
+    private const int Layer9 = 17;
+    private const int Layer10 = 18;
+    private const int Layer11 = 19;
+    private const int Layer12 = 21;
+    private const int Layer13 = 23;
+    private const int Layer14 = 24;
+    private const int Layer15 = 26;
+    private const int Layer16 = 27;
+    private const int Layer17 = 31;
+    static int _layermask1 = 1 << Layer1;
+    static int _layermask2 = 1 << Layer2; 
+    static int _layermask3 = 1 << Layer3;
+    static int _layermask4 = 1 << Layer4;
+    static int _layermask5 = 1 << Layer5;
+    static int _layermask6 = 1 << Layer6;
+    static int _layermask7 = 1 << Layer7;
+    static int _layermask8 = 1 << Layer8;
+    static int _layermask9 = 1 << Layer9;
+    static int _layermask10 = 1 << Layer10;
+    static int _layermask11 = 1 << Layer11;
+    static int _layermask12 = 1 << Layer12;
+    static int _layermask13 = 1 << Layer13;
+    static int _layermask14 = 1 << Layer14;
+    static int _layermask15 = 1 << Layer15;
+    static int _layermask16 = 1 << Layer16;
+    static int _layermask17 = 1 << Layer17;
+
+    static int _finalmask = ~(_layermask1 | _layermask2 | _layermask3 | _layermask4 | _layermask5 | _layermask6 |
+                              _layermask7 |
+                              _layermask8 | _layermask9 | _layermask10 | _layermask11 | _layermask12 | _layermask13 |
+                              _layermask14 | _layermask15 | _layermask16 | _layermask17);
+
+    private static Transform visualEffect = null!;
+
+    static void Postfix(Hud __instance, Player player, float bowDrawPercentage)
+    {
+        if (!ObjectPingPlugin._keyboardShortcut.Value.IsDown()) return;
+        _cam = GameCamera.instance.m_camera;
+        if (_cam?.transform == null) return;
+        Transform? transform = _cam?.transform;
+        _cam.ScreenPointToRay(transform.position);
+        if (!Physics.Raycast(transform.position, transform.forward, out RaycastHit raycastHit, Mathf.Infinity,
+                _finalmask)) return;
+        Vector3 point = raycastHit.point;
+        ObjectPingPlugin.ObjectPingLogger.LogDebug(
+            $"You targeted {raycastHit.collider.transform.root.gameObject.name.Replace("(Clone)", "")}");
+        Object.Instantiate(
+            ObjectPingPlugin.Placementmarkercopy, point,
+            Quaternion.identity);
+
+        GameObject fetch = ZNetScene.instance.GetPrefab("vfx_sledge_hit");
+        Transform fetch2 = fetch.transform.Find("waves");
+        visualEffect = Object.Instantiate(fetch2);
+
+
+        Transform? test = Object.Instantiate(
+            visualEffect, point,
+            Quaternion.identity);
+        test.gameObject.AddComponent<TimedDestruction>();
+        TimedDestruction? t = test.gameObject.GetComponent<TimedDestruction>();
+        t.m_triggerOnAwake = true;
+        t.m_timeout = 5f;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,125 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using ServerSync;
+using UnityEngine;
+
+namespace ObjectPing
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class ObjectPingPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "ObjectPing";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource ObjectPingLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private static readonly ConfigSync ConfigSync = new(ModGUID)
+            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+        internal static GameObject? Placementmarkercopy;
+
+        public void Awake()
+        {
+            ConfigSync.IsLocked = true;
+
+            _keyboardShortcut = config("1 - General", "Ping Keyboard Shortcut", KeyboardShortcut.Empty,
+                "Set up your keys you'd like to use to trigger the ping.", false);
+
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                ObjectPingLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                ObjectPingLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                ObjectPingLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+
+        #region ConfigOptions
+
+        // private static ConfigEntry<bool> _serverConfigLocked = null!;
+        internal static ConfigEntry<KeyboardShortcut> _keyboardShortcut = null!;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            public bool? Browsable = false;
+        }
+
+        class AcceptableShortcuts : AcceptableValueBase
+        {
+            public AcceptableShortcuts() : base(typeof(KeyboardShortcut))
+            {
+            }
+
+            public override object Clamp(object value) => value;
+            public override bool IsValid(object value) => true;
+
+            public override string ToDescriptionString() =>
+                "# Acceptable values: " + string.Join(", ", KeyboardShortcut.AllKeyCodes);
+        }
+
+        #endregion
+    }
+}

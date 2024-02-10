2e15740c96ce17f9f5e80f50b35992048ae30db3
AzumattDev <azumattgaming@gmail.com>
2024-02-10 05:54:52 -0500

    v2.0.6

---
+++ PingDestruction.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+
+namespace ObjectPing;
+
+public class PingDestruction : MonoBehaviour
+{
+    public float m_timeout = 1f;
+    public bool m_triggerOnAwake;
+    public ZNetView m_nview = null!;
+
+    public void Awake()
+    {
+        m_nview = GetComponent<ZNetView>();
+        if (!m_triggerOnAwake)
+            return;
+        Trigger();
+    }
+
+    public void Trigger() => InvokeRepeating(nameof(DestroyNow), m_timeout, 1f);
+
+    public void Trigger(float timeout) => InvokeRepeating(nameof(DestroyNow), timeout, 1f);
+
+    public void OnDestroy()
+    {
+        Cancel();
+        TryDestroyGameObject();
+    }
+
+    public void DestroyNow()
+    {
+        TryDestroyGameObject();
+    }
+
+    public void Cancel() => CancelInvoke(nameof(DestroyNow));
+
+    public void OnDisable() => Cancel();
+
+    private void OnPreCull()
+    {
+        TryDestroyGameObject();
+    }
+
+    private void TryDestroyGameObject()
+    {
+        try
+        {
+            if (m_nview)
+            {
+                if (!m_nview.IsValid() || !m_nview.IsOwner())
+                    return;
+                ZNetScene.instance.Destroy(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        catch (Exception ex)
+        {
+            ObjectPingPlugin.ObjectPingLogger.LogError($"Failed to destroy game object: {ex.Message} {gameObject.name}");
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Reflection;
 using BepInEx;
 using BepInEx.Configuration;
@@ -12,25 +13,21 @@ namespace ObjectPing
     public class ObjectPingPlugin : BaseUnityPlugin
     {
         internal const string ModName = "ObjectPing";
-        internal const string ModVersion = "2.0.5";
+        internal const string ModVersion = "2.0.6";
         internal const string Author = "Azumatt";
         private const string ModGUID = Author + "." + ModName;
         private static string ConfigFileName = ModGUID + ".cfg";
         private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
-
         private readonly Harmony _harmony = new(ModGUID);
-
-        public static readonly ManualLogSource ObjectPingLogger =
-            BepInEx.Logging.Logger.CreateLogSource(ModName);
+        public static readonly ManualLogSource ObjectPingLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
 
         internal static GameObject _placementmarkercopy = null!;
         internal static GameObject _placementmarkerContainer = null!;
 
         public void Awake()
         {
-            _keyboardShortcut = Config.Bind("1 - General", "Ping Keyboard Shortcut", KeyboardShortcut.Empty,
-                new ConfigDescription("Set up your keys you'd like to use to trigger the ping.",
-                    new AcceptableShortcuts()));
+            _keyboardShortcut = Config.Bind("1 - General", "Ping Keyboard Shortcut", KeyboardShortcut.Empty, new ConfigDescription("Set up your keys you'd like to use to trigger the ping.", new AcceptableShortcuts()));
+            TextColor = Config.Bind("1 - General", "Ping Text Color", Color.cyan, new ConfigDescription("Color of the world text when pinging."));
 
             _placementmarkercopy = new GameObject("PingPrefab");
             DontDestroyOnLoad(_placementmarkercopy);
@@ -77,6 +74,8 @@ namespace ObjectPing
 
         // private static ConfigEntry<bool> _serverConfigLocked = null!;
         internal static ConfigEntry<KeyboardShortcut> _keyboardShortcut = null!;
+        internal static ConfigEntry<Color> TextColor = null!;
+
         class AcceptableShortcuts : AcceptableValueBase
         {
             public AcceptableShortcuts() : base(typeof(KeyboardShortcut))
@@ -86,10 +85,24 @@ namespace ObjectPing
             public override object Clamp(object value) => value;
             public override bool IsValid(object value) => true;
 
-            public override string ToDescriptionString() =>
-                "# Acceptable values: " + string.Join(", ", KeyboardShortcut.AllKeyCodes);
+            public override string ToDescriptionString() => "# Acceptable values: " + string.Join(", ", UnityInput.Current.SupportedKeyCodes);
         }
 
         #endregion
+    }
+
+    public static class KeyboardExtensions
+    {
+        // thank you to 'Margmas' for giving me this snippet from VNEI https://github.com/MSchmoecker/VNEI/blob/master/VNEI/Logic/BepInExExtensions.cs#L21
+        // since KeyboardShortcut.IsPressed and KeyboardShortcut.IsDown behave unintuitively
+        public static bool IsKeyDown(this KeyboardShortcut shortcut)
+        {
+            return shortcut.MainKey != KeyCode.None && Input.GetKeyDown(shortcut.MainKey) && shortcut.Modifiers.All(Input.GetKey);
+        }
+
+        public static bool IsKeyHeld(this KeyboardShortcut shortcut)
+        {
+            return shortcut.MainKey != KeyCode.None && Input.GetKey(shortcut.MainKey) && shortcut.Modifiers.All(Input.GetKey);
+        }
     }
 }
